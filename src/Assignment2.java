f43d49a5be6f89b1a0c8884b370a6a88cbe3f528
Andrew.M <mugisaandrew635@gmail.com>
2026-08-09 17:23:55 -0400

    first commit

---
+++ src/Assignment2.java
@@ -0,0 +1,55 @@
+package V1;
+
+public class Assignment2 {
+
+
+    public static void main(String[] args) {
+
+        //keep prompting user until they press 0 to exit/ quit the program
+
+        System.out.println("\nWelcome to the encryption tool");
+
+        EncryptionTool encryptionTool = new EncryptionTool();
+
+        while (true) {
+
+            //get the initial user option/choice
+            int option = encryptionTool.getMenuItem();
+
+            //take them to the selected option workflow and throw exception if anything goes wrong
+            switch (option) {
+                case 0:
+                    //exit the program
+                    System.out.print("Program written by Andrew Mugisa");
+                    System.exit(0);
+                    //return;
+                case 1: //encrypt
+                    /**
+                     * Here the user will be able to encrypt text
+                     * let the user select between ceasar and vigenere
+                     * Encrypt and output the encrypted text for each case
+                     */
+
+                    System.out.print("\nEncrypted text= " + encryptionTool.encryptText() + "\n");
+                    break;
+
+                case 2: //decrypt
+                    /**
+                     * Here the user will be able to decrypt text
+                     * let the user select between ceasar and vigenere
+                     * Decrypt and output the decrypted text for each case
+                     */
+
+                    System.out.print("\nDecrypted text= " + encryptionTool.decryptText() + "\n");
+                    break;
+                case 3:
+                    encryptionTool.encryptFile();
+                    break;
+                case 4:
+                    encryptionTool.decryptFile();
+                    break;
+            }
+        }
+
+    }
+}
---
+++ src/Caesar.java
@@ -0,0 +1,60 @@
+package V1; /**
+ * Student Name: Andrew Mugisa
+ * Student Number:
+ * Course: CST8132 Object Oriented Programming
+ * Program: CET-CS-Level 2
+ * Lab Professor: Howard Rosenblum
+ */
+
+/**
+ * This is a basic encryption method
+ * *It will take in a string and encrypt it by shifting characters
+ */
+public class Caesar extends Cipher {
+    private final int shift;
+
+    public Caesar(int shift) {
+        this.shift = shift;
+    }
+
+    @Override
+    public String encrypt(String text) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+
+            /**
+             * If char is less than first char --> append char to result
+             * If char is greater than last char ---> append character to result
+             *
+             * else
+             * Shift chars and append to result
+             */
+            if ((ch < Cipher.FIRST_CHAR) || (ch > Cipher.LAST_CHAR)) {
+//               if(ch < FIRST_CHAR){result.append(ch);}
+//               if(ch > LAST_CHAR){result.append(ch);}
+                result.append(ch);
+            } else {
+                result.append((char) ((ch + shift - Cipher.FIRST_CHAR + Cipher.CHAR_DIFF) % Cipher.CHAR_DIFF + Cipher.FIRST_CHAR));
+            }
+        }
+        return result.toString();
+    }
+
+    @Override
+    public String decrypt(String text) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if ((ch < Cipher.FIRST_CHAR) || (ch > Cipher.LAST_CHAR)) {
+                result.append(ch);
+            } else {
+                result.append((char) ((ch - shift - Cipher.FIRST_CHAR + Cipher.CHAR_DIFF) % Cipher.CHAR_DIFF + Cipher.FIRST_CHAR));
+            }
+
+        }
+        return result.toString();
+    }
+
+}
